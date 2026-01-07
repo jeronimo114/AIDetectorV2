2fdf08edcfa8580f477385f00dc1cdac02076155
Jeronimo114 <jeronimomartinezjaramillo@gmail.com>
2026-01-07 11:24:10 -0500

    Add Google Analytics tag default

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -2,7 +2,7 @@
 
 import Script from "next/script";
 
-const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID ?? "G-RPKNR2XQGW";
 
 export default function GoogleAnalytics() {
   if (!GA_MEASUREMENT_ID) {
